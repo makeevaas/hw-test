49cf2934fcbd3745010e116aae2afac4d5f92a23
Anastasia Makeeva <makeeva-a-s@mail.ru>
2025-01-10 17:24:56 +0300

    expanded test configurations

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,4 +1,4 @@
-package unpack
+package hw02unpackstring
 
 import (
 	"errors"
@@ -13,10 +13,31 @@ func Unpack(s string) (string, error) {
 	// Place your code here.
 	var newStr string
 	var elCurrent, elNext int
+	var elBack string
+	var separator bool
 	for i, r := range s {
+		//условия пропусков
 		if r >= 128 {
 			continue
 		}
+		if i > 0 && len(s) >= i+2 && string(r) == `\` && string(s[i+1]) == `\` && string(s[i+2]) == `\` {
+			separator = true
+			newStr += string(r)
+		}
+		if string(r) != `\` {
+			separator = false
+		}
+		if i > 0 && elBack != string(s[i-1]) {
+			elBack = ""
+		}
+		if string(r) == `\` && elBack != `\` {
+			elBack = `\`
+			continue
+		}
+		if separator {
+			continue
+		}
+
 		// текущий элемент
 		elCurrent = i
 		// следующий
@@ -27,7 +48,9 @@ func Unpack(s string) (string, error) {
 			fmt.Println("составное число: ", checkSteamNum)
 			_, err := strconv.Atoi(checkSteamNum)
 			if err == nil {
-				return "", ErrInvalidString
+				if elBack != `\` {
+					return "", ErrInvalidString
+				}
 			}
 		} else if i+1 > len(s) {
 			elNext = i
@@ -43,15 +66,22 @@ func Unpack(s string) (string, error) {
 		}
 
 		// если ошибок не произошло - делаем строку новую
+
+		fmt.Printf("%v\n", separator)
+
 		in, err := strconv.Atoi(string(s[elNext]))
 		if err == nil {
 			if elCurrent != elNext {
 				fmt.Println(in)
 				newStr += strings.Repeat(string(s[elCurrent]), in)
+
 			}
 		} else {
 			_, err := strconv.Atoi(string(r))
 			if err != nil {
+				newStr += string(r)
+			}
+			if elBack == `\` {
 				newStr += string(r)
 			}
 		}
