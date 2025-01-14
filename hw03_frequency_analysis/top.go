4e2aa879786e405b52ba8b6c22f6a867057f6372
Anastasia Makeeva <makeeva-a-s@mail.ru>
2025-01-15 01:35:21 +0300

    add go func find top10 words

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,15 +1,53 @@
-// package hw03frequencyanalysis
-package main
+package hw03frequencyanalysis
 
-import "fmt"
+import (
+	"sort"
+	"strings"
+)
+
+var NotWord = "-"
 
 func Top10(s string) []string {
 	// Place your code here.
-	var sLst []string
-	sLst = append(sLst, s)
-	return sLst
-}
-func main() {
-	res := Top10("привет")
-	fmt.Println(res)
+	s = strings.ToLower(s)
+	var lstWords []string
+	wordLsts := make([][]string, 0, len(s))
+	// s := "aligator dog cat cat aligator mouth cat dog dog cat dog aligator mouth horse aligator"
+	// top 10
+	newSlice := strings.Fields(s)
+	sort.Strings(sort.StringSlice(newSlice))
+	var word string
+	for _, r := range newSlice {
+		r = strings.ReplaceAll(r, ".", "")
+		if r == NotWord {
+			continue
+		}
+		if r != word {
+			word = r
+		} else {
+			continue
+		}
+		lstWords = nil
+		for _, r := range newSlice {
+			r = strings.ReplaceAll(r, ".", "")
+			if r == word {
+				lstWords = append(lstWords, r)
+			}
+		}
+		wordLsts = append(wordLsts, lstWords)
+	}
+	// определить большинство
+	sort.SliceStable(wordLsts, func(i, j int) bool {
+		return len(wordLsts[i]) > len(wordLsts[j])
+	})
+	// вывести топ 10
+	top10Str := make([]string, 0, 9)
+	top := 10
+	for i, r := range wordLsts {
+		if i == top {
+			break
+		}
+		top10Str = append(top10Str, r[0])
+	}
+	return top10Str
 }
