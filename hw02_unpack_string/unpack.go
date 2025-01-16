95af74b86b16b19abed1d94605cfc1fc47bbd19f
Anastasia Makeeva <makeeva-a-s@mail.ru>
2025-01-16 13:19:39 +0300

    unificate

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,18 +2,21 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrInvalidString = errors.New("invalid string")
+	ByteDelimiter    = []byte(`\`)[0]
+)
 
 func Unpack(s string) (string, error) {
 	// Place your code here.
-	var newStr string
+	var newStr strings.Builder
 	var elCurrent, elNext int
-	var elBack string
+	var elBack byte
 	var continueIterValue, delimiter bool
 
 	for i, r := range s {
@@ -21,96 +24,86 @@ func Unpack(s string) (string, error) {
 		if r >= 128 {
 			continue
 		}
-
 		delimiter = checkDelimiterEl(r, delimiter)
 
-		delimiter, newStr = checkDelimiterSet(i, s, newStr, delimiter, r)
+		if checkDelimiterSet(i, s, r) {
+			delimiter = true
+			newStr.WriteString(string(r))
+		}
 
 		elBack = resetLastValue(i, s, elBack)
-
 		continueIterValue, elBack = continueIter(elBack, r, delimiter)
+
 		if continueIterValue {
 			continue
 		}
-
 		// текущий элемент
 		elCurrent = i
 		// следующий
 		if i+1 < len(s) {
 			elNext = i + 1
 			// если число составное = ошибка
-			checkSteamNum := string(r) + string(s[elNext])
-			fmt.Println("составное число: ", checkSteamNum)
-			_, err := strconv.Atoi(checkSteamNum)
-			if err == nil {
-				if elBack != `\` {
-					return "", ErrInvalidString
-				}
+			if verifyComNum(r, rune(s[elNext]), elBack) {
+				return "", ErrInvalidString
 			}
 		} else if i+1 > len(s) {
 			elNext = i
 		}
-		// предыдущий
-		if i == 0 {
-			// если первый лемент числовой = ошибка
-			fmt.Println("первый элемент строки: ", string(r))
-			_, err := strconv.Atoi(string(r))
-			if err == nil {
-				return "", ErrInvalidString
-			}
+
+		// если первый лемент числовой = ошибка
+		if i == 0 && unicode.IsDigit(r) {
+			return "", ErrInvalidString
 		}
 
 		// если ошибок не произошло - делаем строку новую
-		in, err := strconv.Atoi(string(s[elNext]))
-		if err == nil {
+		if unicode.IsDigit(rune(s[elNext])) {
+			in, _ := strconv.Atoi(string(s[elNext]))
 			if elCurrent != elNext {
-				fmt.Println(in)
-				newStr += strings.Repeat(string(s[elCurrent]), in)
+				newStr.WriteString(strings.Repeat(string(s[elCurrent]), in))
 			}
 		} else {
-			_, err := strconv.Atoi(string(r))
-			if err != nil {
-				newStr += string(r)
-			}
-			if elBack == `\` {
-				newStr += string(r)
+			if !unicode.IsDigit(r) {
+				newStr.WriteString(string(r))
+			} else if elBack == ByteDelimiter {
+				newStr.WriteString(string(r))
 			}
 		}
-
-		fmt.Println("cur - ", elCurrent)
-		fmt.Println("next -", elNext)
-		fmt.Println("===============")
 	}
-	fmt.Printf("Новая строка:%s %T\n", newStr, newStr)
-	return newStr, nil
+	return newStr.String(), nil
+}
+
+func verifyComNum(r, rNext rune, elBack byte) bool {
+	if unicode.IsDigit(r) && unicode.IsDigit(rNext) && elBack != ByteDelimiter {
+		return true
+	}
+	return false
 }
 
 func checkDelimiterEl(r rune, delimiter bool) bool {
-	if string(r) != `\` {
+	if byte(r) != ByteDelimiter {
 		delimiter = false
 	}
 	return delimiter
 }
 
-func checkDelimiterSet(i int, s, newStr string, delimiter bool, r rune) (bool, string) {
-	if i > 0 && len(s) >= i+2 && string(r) == `\` && string(s[i+1]) == `\` && string(s[i+2]) == `\` {
-		delimiter = true
-		newStr += string(r)
+func checkDelimiterSet(i int, s string, r rune) bool {
+	if i > 0 && len(s) >= i+2 && byte(r) == ByteDelimiter && s[i+1] == ByteDelimiter && s[i+2] == ByteDelimiter {
+		return true
 	}
-	return delimiter, newStr
+	return false
 }
 
-func resetLastValue(i int, s, elBack string) string {
-	if i > 0 && elBack != string(s[i-1]) {
-		elBack = ""
+func resetLastValue(i int, s string, elBack byte) byte {
+	if i > 0 && elBack != s[i-1] {
+		elBack = 0
 	}
 	return elBack
 }
 
-func continueIter(elBack string, r rune, delimiter bool) (bool, string) {
+func continueIter(elBack byte, r rune, delimiter bool) (bool, byte) {
 	var res bool
-	if string(r) == `\` && elBack != `\` {
-		elBack = `\`
+	if byte(r) == ByteDelimiter && elBack != ByteDelimiter {
+		elBack = ByteDelimiter
 		res = true
 	}
 	if delimiter {
